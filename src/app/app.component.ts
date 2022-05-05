e5be6d51f2d9118375a684b2a1075c4efcee3bf9
uhcoronaa <uhcorona@gmail.com>
2022-05-05 15:44:13 -0600

    Fixes loader overlay error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { UsersService } from './services/users.service';
 import * as userActions from './users/state/users.actions';
 import * as loaderSelectors from './specific/loader/loader.selectors';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'my-notes-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
+  subscriptions: Subscription[] = [];
+  overlayEnabled: boolean = false;
   loadersObservable = this.store.select(loaderSelectors.loadings);
 
   constructor(private store: Store, private userService: UsersService) { }
@@ -22,5 +25,17 @@ export class AppComponent implements OnInit {
       this.store.dispatch(userActions.accessTokenUpdated({ accessToken: userCredentials.accessToken }));
       this.store.dispatch(userActions.refreshTokenUpdated({ refreshToken: userCredentials.refreshToken }));
     }
+    this.subscriptions.push(this.loadersObservable.subscribe((loader) => {
+      setTimeout(() => {
+        this.overlayEnabled = !!loader;
+      });
+    }));
   }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => {
+      s.unsubscribe();
+    });
+  }
+
 }
