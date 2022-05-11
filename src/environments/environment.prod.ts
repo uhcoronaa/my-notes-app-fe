87075c64ad5a62b381d5b8e2590a87d948f2b322
uhcoronaa <uhcorona@gmail.com>
2022-05-10 20:59:53 -0600

    Updates env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  users: 'http://localhost:3000/api/users',
-  categories: 'http://localhost:3000/api/categories',
-  notes: 'http://localhost:3000/api/notes',
+  apiUrl: 'http://localhost:8081/api',
+  users: 'http://localhost:8081/api/users',
+  categories: 'http://localhost:8081/api/categories',
+  notes: 'http://localhost:8081/api/notes',
 };
